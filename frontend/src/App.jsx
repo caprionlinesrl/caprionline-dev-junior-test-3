e12a0b09a12ac7d46c00f1aafa23ac100b65fb3d
carmelocammalleri <carmecamma1995@gmail.com>
2024-05-13 12:00:15 +0200

    [add]: form to search film & chang faviconame

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -67,66 +67,78 @@ const MovieList = props => {
   }
 
   return (
-    <div className="grid gap-4 md:gap-y-8 xl:grid-cols-6 lg:grid-cols-4 md:grid-cols-3">
-      {props.children}
-    </div>
+    <>
+      {/* aggiungo un form per la ricerca dei film tramite nome */}
+      <form action="" className='mt-2 mb-2'>
+          <input type="text" className='text-gray-700 text-sm font-bold mb-2 me-2'/>
+          <button type='submit' className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded'>Search</button>
+          <p>{console.log(props.children[0])}</p>
+      </form>
+      {/* //aggiungo un form per la ricerca dei film tramite nome// */}
+
+      <div className="grid gap-4 md:gap-y-8 xl:grid-cols-6 lg:grid-cols-4 md:grid-cols-3">
+        {props.children}
+      </div>
+    </>
   );
 };
 
 const MovieItem = props => {
   return (
-    <div className="flex flex-col w-full h-full rounded-lg shadow-md lg:max-w-sm">
-      <div className="grow">
-        <img
-          className="object-cover w-full h-60 md:h-80"
-          src={props.imageUrl}
-          alt={props.title}
-          loading="lazy"
-        />
-      </div>
-
-      <div className="grow flex flex-col h-full p-3">
-        <div className="grow mb-3 last:mb-0">
-          {props.year || props.rating
-            ? <div className="flex justify-between align-middle text-gray-900 text-xs font-medium mb-2">
-                <span>{props.year}</span>
-
-                {props.rating
-                  ? <Rating>
-                      <Rating.Star />
-
-                      <span className="ml-0.5">
-                        {props.rating}
-                      </span>
-                    </Rating>
-                  : null
-                }
-              </div>
-            : null
-          }
-
-          <h3 className="text-gray-900 text-lg leading-tight font-semibold mb-1">
-            {props.title}
-          </h3>
-
-          <p className="text-gray-600 text-sm leading-normal mb-4 last:mb-0">
-            {props.plot.substr(0, 80)}...
-          </p>
+    <>
+      <div className="flex flex-col w-full h-full rounded-lg shadow-md lg:max-w-sm">
+        <div className="grow">
+          <img
+            className="object-cover w-full h-60 md:h-80"
+            src={props.imageUrl}
+            alt={props.title}
+            loading="lazy"
+          />
         </div>
 
-        {props.wikipediaUrl
-          ? <Button
-              color="light"
-              size="xs"
-              className="w-full"
-              onClick={() => window.open(props.wikipediaUrl, '_blank')}
-            >
-              More
-            </Button>
-          : null
-        }
+        <div className="grow flex flex-col h-full p-3">
+          <div className="grow mb-3 last:mb-0">
+            {props.year || props.rating
+              ? <div className="flex justify-between align-middle text-gray-900 text-xs font-medium mb-2">
+                  <span>{props.year}</span>
+
+                  {props.rating
+                    ? <Rating>
+                        <Rating.Star />
+
+                        <span className="ml-0.5">
+                          {props.rating}
+                        </span>
+                      </Rating>
+                    : null
+                  }
+                </div>
+              : null
+            }
+
+            <h3 className="text-gray-900 text-lg leading-tight font-semibold mb-1">
+              {props.title}
+            </h3>
+
+            <p className="text-gray-600 text-sm leading-normal mb-4 last:mb-0">
+              {props.plot.substr(0, 80)}...
+            </p>
+          </div>
+
+          {props.wikipediaUrl
+            ? <Button
+                color="light"
+                size="xs"
+                className="w-full"
+                onClick={() => window.open(props.wikipediaUrl, '_blank')}
+              >
+                More
+              </Button>
+            : null
+          }
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
